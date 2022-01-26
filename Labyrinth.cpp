5fd1da9e7218ceb4b291ea3c42742e10a4335fb1
sebstian0627 <choudharyshreyansh27@gmail.com>
2022-01-27 01:17:40 +0530

    Labyrinth finished

--- Labyrinth.cpp
+++ Labyrinth.cpp
@@ -1,111 +1,76 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-#define first f
-#define second s
-#define F(i,n) for(int i=0;i<(int)n;i++)
-#define pb push_back
-#define pii pair<int,int>
-#define vpii vector<pii>
+#define ii pair<int, int>
 #define f first
 #define s second
-#define print(x) cout << x << endl;
-#define print_array(arr) F(i, arr.size()){cout << arr[i] << " ";}cout << endl;
+#define mp make_pair
 
+int n, m;
+char A[1000][1000];
+bool vis[1000][1000];
 
-struct node{
-  int i,j,len;
-  string hist;
+// previousStep stores the previous direction that we moved in to arrive that this cell
+int previousStep[1000][1000];
 
-  node(int x, int y, int l, string h){
-    i=x;j=y;len=l;hist=h;
-  }
-};
+// 0 = up, 1 = right, 2 = down, 3 = left
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+string stepDir = "URDL";
 
-bool check(int i,int j,int n, int m){
-  return i>=0&&j>=0&&i<n&&j<m;
-}
+int main() {
+	cin >> n >> m;
 
-int main(){
-  int n,m;
-  cin>>n>>m;
-  vector<vector<char>> v(n,vector<char>(m,' '));
+	queue<ii> q;
+	ii begin, end;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> A[i][j];
+			if (A[i][j] == 'A') {
+				begin = mp(i, j);
+			} else if (A[i][j] == 'B') {
+				end = mp(i, j);
+			}
+		}
+	}
 
-  int Ax=0;
-  int Ay=0;
-  int Bx=0;
-  int By=0;
+	q.push(begin);
+	vis[begin.f][begin.s] = true;
 
+	while (!q.empty()) {
+		ii u = q.front(); q.pop();
+		for (int i = 0; i < 4; i++) {
+			ii v = mp(u.f + dx[i], u.s + dy[i]);
+			if (v.f < 0 || v.f >= n || v.s < 0 || v.s >= m) continue;
+			if (A[v.f][v.s] == '#') continue;
+			if (vis[v.f][v.s]) continue;
+			vis[v.f][v.s] = true;
+			previousStep[v.f][v.s] = i;
+			q.push(v);
+		}
+	}
 
-  for(int i=0;i<n;i++){
-    for(int j=0;j<m;j++){
-      cin>>v[i][j];
-      if(v[i][j]=='A'){
-        Ax=i;Ay=j;
-      }
-      if(v[i][j]=='B'){
-        Bx=i;By=j;
-      }
-    }
-  }
-  // print(Bx);
-  // for(int i=0;i<n;i++){
-  //   for(int j=0;j<m;j++){
-  //     print(v[i][j]);
-  //   }
-  // }
+	if (vis[end.f][end.s]) {
+		cout << "YES" << endl;
+		vector<int> steps;
+		while (end != begin) {
+			int p = previousStep[end.f][end.s];
+			steps.push_back(p);
+			// undo the previous step to get back to the previous square
+			// Notice how we subtract dx/dy, whereas we added dx/dy before
+			end = mp(end.f - dx[p], end.s - dy[p]);
+		}
+		reverse(steps.begin(), steps.end());
 
-  vector<vector<bool>> visited(n, vector<bool>(m,false));
+		cout << steps.size() << endl;
+		for (char c : steps) {
+			cout << stepDir[c];
+		}
+		cout << endl;
+	} else {
+		cout << "NO" << endl;
+	}
 
-  queue<node>q;
-  node nn(Ax,Ay,0,"");
-  q.push(nn);
-  visited[Ax][Ay]=true;
-  while(q.size()){
-    node nod  = q.front();q.pop();
-    int l = nod.len;
-    string h= nod.hist;
-    int i=nod.i;
-    int j = nod.j;
-    if(i==Bx && j==By){
-      cout<<"YES"<<endl;
-      cout<<l<<endl;
-      cout<<h<<endl;
-      return 0;
-    }
-    if(check(i-1,j,n,m) && !visited[i-1][j] && (v[i-1][j]=='.' || v[i-1][j]=='B')){
-      node temp(i-1,j,l+1, h+"U");
-      q.push(temp);
-      visited[i-1][j]=true;
-
-    }
-    if(check(i+1,j,n,m) && !visited[i+1][j] && (v[i+1][j]=='.'||v[i+1][j]=='B')){
-      node temp(i+1,j,l+1, h+"D");
-      q.push(temp);
-      visited[i+1][j]=true;
-
-    }
-    if(check(i,j-1,n,m) && !visited[i][j-1] && (v[i][j-1]=='.'||v[i][j-1]=='B')){
-      // print("HELLO");
-      node temp(i,j-1,l+1, h+"L");
-      q.push(temp);
-      visited[i][j-1]=true;
-
-    }
-    if(check(i,j+1,n,m) && !visited[i][j+1] && (v[i][j+1]=='.'||v[i][j-1]=='B')){
-      node temp(i,j+1,l+1, h+"R");
-      q.push(temp);
-      visited[i][j+1]=true;
-
-    }
-
-
-  }
-
-  cout<<"NO\n";
-
-
-
-
-
+	return 0;
 }
